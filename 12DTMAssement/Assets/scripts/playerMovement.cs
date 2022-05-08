54b8fbc567b7a4772f1c6a3bc63723c707f62d63
talkingdimwit <19037@wc.school.nz>
2022-05-09 00:36:52 +1200

    GAME FINISHED!!!!!!!
    version 1.0 you could say.
    everything is in

--- 12DTMAssement/Assets/scripts/playerMovement.cs
+++ 12DTMAssement/Assets/scripts/playerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 using System;
 
@@ -9,25 +10,32 @@ public class playerMovement : MonoBehaviour
     // Varibles
     public float speed = 20f;
     public bool isOnground = true;
+    public bool gameOver = false;
     public Rigidbody2D rb;
     public LineRenderer linerenderer;
-    private Vector2 initialVilocity;
-    private Vector2 mouseCorrection;
     private int lineScale = 10;
     private bool havePower = false;
-    private float jumpCount = 0;
     private float time = 0;
     private float roundSeconds;
+    private float deathCount = 0;
+    private float collectables = 0;
     private Vector2 spawnLocation;
     Vector2 lastClickedPos;
-    public TextMeshProUGUI jumpCountText;
+    private Vector2 initialVilocity;
+    private Vector2 mouseCorrection;
+    public TextMeshProUGUI deathCountText;
     public TextMeshProUGUI timerText;
+    public TextMeshProUGUI winText;
+    public TextMeshProUGUI scoresText;
 
     // Start is called before the first frame update
     void Start()
     {
         linerenderer.positionCount = lineScale;
         mouseCorrection.y = 5;
+        winText.text = "";
+        scoresText.text = "";
+        spawnLocation = new Vector2(24, 1021);
     }
 
     // Detects when the player leaves and touches the ground
@@ -45,6 +53,7 @@ public class playerMovement : MonoBehaviour
             rb.constraints = RigidbodyConstraints2D.FreezeAll;
             rb.constraints = RigidbodyConstraints2D.None;
             rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+            deathCount += 1;
         }
     }
     public void OnCollisionExit2D(Collision2D collision)
@@ -79,22 +88,45 @@ public class playerMovement : MonoBehaviour
             spawnLocation = new Vector2(rb.position.x, rb.position.y);
             Time.timeScale = 1f;
         }
+        if (other.CompareTag("collectable"))
+        {
+            other.gameObject.active = false;
+            collectables += 1;
+        }
+        if (other.CompareTag("end"))
+        {
+            gameOver = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (gameOver == false)
+        {
+            deathCountText.text = "Deaths: " + deathCount;
+            winText.text = "";
+            scoresText.text = "";
+            Timer();
+        }
+        if (gameOver == true)
+        {
+            deathCountText.text = "";
+            timerText.text = "";
+            winText.text = "You Win\n\n\nPress Space To Restart";
+            scoresText.text = "time: " + roundSeconds + "s" + "\ndeaths: " + deathCount + "\ncollectables: " + collectables + "/3";
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+            linerenderer.enabled = false;
+            if (Input.GetKeyDown("space"))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+
         // Gets mouse position then makes the player be thrown towards it
         lastClickedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         initialVilocity = (lastClickedPos + mouseCorrection) - rb.position;
         LineUpdate();
-
-        time += Time.deltaTime;
-        roundSeconds = (float)Math.Round(time, 0);
-        timerText.text = "Time: " + roundSeconds;
-
-        jumpCountText.text = "Jumps: " + jumpCount;
-
         if (Input.GetMouseButtonDown(0) && isOnground)
             {
                 Time.timeScale = 0.5f; // slows time on click hold
@@ -113,7 +145,14 @@ public class playerMovement : MonoBehaviour
             rb.constraints = RigidbodyConstraints2D.None;
             rb.constraints = RigidbodyConstraints2D.FreezeRotation;
             rb.AddForce(initialVilocity * speed, ForceMode2D.Impulse); //applies thurst to player
-            jumpCount += 1;
+        }
+
+        if (Input.GetKeyDown("r"))
+        {
+            rb.position = new Vector2(spawnLocation.x, spawnLocation.y); // Sends player to checkpoint if they touch lava
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+            rb.constraints = RigidbodyConstraints2D.None;
+            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
         }
     }
 
@@ -136,5 +175,12 @@ public class playerMovement : MonoBehaviour
             linerenderer.SetPosition(i, pos);
             fTime += timeStep;
         }
+    }
+    // counts how long the player has
+    private void Timer()
+    {
+        time += Time.deltaTime;
+        roundSeconds = (float)Math.Round(time, 0);
+        timerText.text = "Time: " + roundSeconds + "s";
     }
 }
